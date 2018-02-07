1c883c21614e2753911cebbc2bed0ffc7597578c
RPolski <polskirob@gmail.com>
2018-02-07 13:35:19 -0800

    cleaned formatting

--- QD.py
+++ QD.py
@@ -16,19 +16,25 @@ from qcodes import Instrument
 import qcodes.utils.validators as vals
 import time
 
+
 class QDInstrument:
     """ The instrument class that calls on the PPMS through MultiVu. Don't
-    use these commands for QCoDeS unless you want the error codes and status codes and such.
+    use these commands for QCoDeS unless you want the error codes and
+    status codes and such.
+
     It has the
     following functions:
-        set_temperature(temp, rate, mode): returns error code. Mode 0 is fast settle.
+        set_temperature(temp, rate, mode): returns error code.
+                Mode 0 is fast settle.
                 Mode 1 is No Overshoot
         get_temperature(): returns error code, temp, status
-        set_field(field, rate, approach, mode): returns error. Approach 0 is linear,
+        set_field(field, rate, approach, mode): returns error.
+                Approach 0 is linear,
                 Mode 0 works (fast settle?)
         get_field(): returns error, field, status. Status 4 is stable
         set_chamber(code): Returns error. Not sure how the codes work here.
-        get_chamber(): returns error, status. Not sure how these work either."""
+        get_chamber(): returns error, status. Not sure how these work either.
+        """
     def __init__(self, instrument_type):
         instrument_type = instrument_type.upper()
         if instrument_type == 'DYNACOOL':
@@ -40,11 +46,13 @@ class QDInstrument:
         elif instrument_type == 'MPMS3':
             self._class_id = 'QD.MULTIVU.MPMS3.1'
         else:
-            raise Exception('Unrecognized instrument type: {0}.'.format(instrument_type))
+            raise Exception('Unrecognized instrument type: ' +
+                            '{0}'.format(instrument_type))
         self._mvu = win32com.client.Dispatch(self._class_id)
 
     def set_temperature(self, temperature, rate, mode):
-        """Sets temperature and returns MultiVu error code. Mode 0 is Fast Settle. Mode 1 is No Overshoot"""
+        """Sets temperature and returns MultiVu error code.
+        Mode 0 is Fast Settle. Mode 1 is No Overshoot"""
         err = self._mvu.SetTemperature(temperature, rate, mode)
         return err
 
@@ -61,22 +69,26 @@ class QDInstrument:
             13: Disabled
             14: Impedance Not Function
             15: Temp Failure"""
-        arg0 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_R8, 0.0)
+        arg0 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_R8,
+                                       0.0)
         arg1 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_I4, 0)
         err = self._mvu.GetTemperature(arg0, arg1)
         # win32com reverses the arguments, so:
         return err, arg1.value, arg0.value
 
     def set_field(self, field, rate, approach, mode):
-        """Sets field and returns MultiVu error code. Approach 1 is No Overshoot. Approach 0 is Linear. Mode 0 works"""
+        """Sets field and returns MultiVu error code.
+        Approach 1 is No Overshoot. Approach 0 is Linear. Mode 0 works"""
         err = self._mvu.SetField(field, rate, approach, mode)
         return err
 
     def get_field(self):
         """Gets and returns field info as (MultiVu error, field, status)
         Status Codes:
-            0: Magnet Unknown (just still needs to be initialized to some value)
-            1: Stable Persistent (This magnet doesn't have a persistent function)
+            0: Magnet Unknown (just still needs to be initialized
+                                to some value)
+            1: Stable Persistent (This magnet doesn't
+                                    have a persistent function)
             2: Warming Switch (not applicable)
             3: Cooling Switch (not applicable)
             4: Stable Driven (This is the stable function for this magnet)
@@ -85,7 +97,8 @@ class QDInstrument:
             7: Discharging (also ramping)
             8: Current Error
             15: Magnet Failure"""
-        arg0 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_R8, 0.0)
+        arg0 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_R8,
+                                       0.0)
         arg1 = win32com.client.VARIANT(pythoncom.VT_BYREF | pythoncom.VT_I4, 0)
         err = self._mvu.GetField(arg0, arg1)
         # win32com reverses the arguments, so:
@@ -102,13 +115,15 @@ class QDInstrument:
         err = self._mvu.GetChamber(arg0)
         return err, arg0.value
 
+
 class QD_System(Instrument, QDInstrument):
     """
-    The actual instrument class to be used by QCoDeS. Note: This uses mT units for magnetic field
+    The actual instrument class to be used by QCoDeS.
+    Note: This uses mT units for magnetic field
     instead of Oersteds.
     """
     def __init__(self, name: str, instrument_type='DYNACOOL',
-                 **kwargs) -> None:
+                 **kwargs):
         """ name: name to use internally in QCodes,
         instrument_type: passed to the previously made QDInstrument script"""
         QDInstrument.__init__(self, instrument_type)
@@ -138,20 +153,24 @@ class QD_System(Instrument, QDInstrument):
                            set_cmd=self.field_set_stable,
                            vals=vals.Numbers(-9000, 9000))
 
-        self.field_rate = 1 # mT/s
-        self.temperature_rate = 10 # K/s
-        print('Note this uses mT units for getting and setting fields with field() and field_set_stable(). Just divide Oe by 10 to get mT')
+        self.field_rate = 1  # mT/s
+        self.temperature_rate = 10  # K/s
+        print('Note this uses mT units for getting and setting fields with' +
+              ' field() and field_set_stable(). Just divide Oe by ' +
+              '10 to get mT')
 
     def temperature_set_stable(self, temperature: Union[int, float],
                                slightlyfaster: bool=False):
-        """ temperature:  in Kelvin. slightlyfaster=True just moves on when the temperature status
+        """ temperature:  in Kelvin.
+        slightlyfaster=True just moves on when the temperature status
         is near instead of requiring it to be stable"""
         err_init, temp_init, status_init = self.get_temperature()
         temp_init = float(temp_init)
         self.set_temperature(temperature, self.temperature_rate, 0)
         waiting = True
         startwaittime = time.time()
-        timeout = (np.abs(temp_init-temperature)/self.temperature_rate)*3 + 240 # in seconds
+        timeout = (np.abs(temp_init-temperature)/self.temperature_rate)*3 + 240
+        # in seconds
         while waiting:
             err, tval, status = self.get_temperature()
             if slightlyfaster:
@@ -175,14 +194,15 @@ class QD_System(Instrument, QDInstrument):
         err, temp, status = self.get_temperature()
         return float(temp)
 
-    def field_set_stable(self, field: Union[int,float]):
+    def field_set_stable(self, field: Union[int, float]):
         """ field: magnetic field in milliTesla"""
         err_init, bval_init, status_init = self.get_field()
-        bval_init = float(bval_init)/10 #convert from Oe to mT
-        self.set_field(field*10, self.field_rate*10, 1, 0) #convert to Oe
+        bval_init = float(bval_init)/10  # convert from Oe to mT
+        self.set_field(field*10, self.field_rate*10, 1, 0)  # convert to Oe
         waiting = True
         startwaittime = time.time()
-        timeout = (np.abs(bval_init-field))/(self.field_rate)*3 + 120 # in seconds
+        timeout = (np.abs(bval_init-field))/(self.field_rate)*3 + 120
+        # in seconds
         while waiting:
             err, bval, status = self.get_field()
             if status == 4:
@@ -195,9 +215,10 @@ class QD_System(Instrument, QDInstrument):
         return
 
     def field_set_release(self, field: Union[int, float]):
-        """ field: magnetic field in milliTesla. Set and don't wait until stable """
-        self.set_field(field*10, self.field_rate*10, 1, 0) #convert to Oe
+        """ field: magnetic field in milliTesla.
+        Set and don't wait until stable """
+        self.set_field(field*10, self.field_rate*10, 1, 0)  # convert to Oe
 
     def field_get_cmd(self):
         err, bval, status = self.get_field()
-        return float(bval/10) #convert from Oe
+        return float(bval/10)  # convert from Oe
