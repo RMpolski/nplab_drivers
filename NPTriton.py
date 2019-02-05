0342da7fa372184d2f8c5de22b74ef81b2d31ec5
RMpolski <polskirob@gmail.com>
2019-02-05 14:39:11 -0800

    fixed parse_outp_bool

--- NPTriton.py
+++ NPTriton.py
@@ -18,11 +18,13 @@ def parse_outp_bool(value):
         value = int(value)
     elif type(value) is str:
         value = value.lower()
-    elif value in {1, 'on', True}:
+
+    if value in {1, 'on', True}:
         return 1
     elif value in {0, 'off', False}:
         return 0
     else:
+        print(value)
         raise ValueError('Must be boolean, on or off, 0 or 1, True or False')
 
 
